bb2724959218f3c3da3557eba774ccc4b2caa482
nigaor <ktnkuhi0116@gmail.com>
2025-04-10 03:12:46 +0900

    4/10 commit

--- monhan_record/src/app/detail/page.tsx
+++ monhan_record/src/app/detail/page.tsx
@@ -1,15 +1,17 @@
+'use client'
+
 import React from 'react';
-import {useRouter} from 'next/router';
+import {useParams,useRouter} from 'next/navigation';
 
 const DetailPage: React.FC = () => {
     const router = useRouter();
-    const {name} = router.query;
+    const {name} = useParams();
 
     return (
         <div>
             <h1>詳細ページ</h1>
             <p>Name:{name}</p>
-            <button onClick={() => router.push('/page')}>
+            <button onClick={() => router.push('/')}>
                 一覧に戻る
             </button>
         </div>
