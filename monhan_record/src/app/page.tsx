76a696bd27a52f58a0e859ac58691125ace22b7d
nigaor <ktnkuhi0116@gmail.com>
2025-04-27 03:58:08 +0900

    4/27 commit

--- monhan_record/src/app/page.tsx
+++ monhan_record/src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     {id: 9,name: "レ・ダウ",img: ""},
     {id: 10,name: "ネルスキュラ",img: ""},
     {id: 11,name: "ヒラバミ",img: ""},
-    // {id: 12,name: "アジャラカン",img: ""},
+    {id: 12,name: "アジャラカン",img: ""},
     // {id: 13,name: "ヌ・エグドラ",img: ""},
     // {id: 14,name: "護竜ドシャグマ",img: ""},
     // {id: 15,name: "護竜リオレウス",img: ""},
